d7cfee35421cf9914e7e5ae82326452ba0fe4707
Z User <z@container>
2026-07-07 01:50:28 +0000

    d7e95a10-f6c0-4752-9052-127cda1a30a5

--- run-dev.sh
+++ run-dev.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /home/z/my-project
 while true; do
-  echo "=== Starting dev server ===" >> /home/z/my-project/dev.log
-  npx next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
-  echo "=== Server exited, restarting in 3s ===" >> /home/z/my-project/dev.log
+  rm -rf .next/cache 2>/dev/null
+  npx next dev -p 3000 2>&1
+  echo "=== Restarting in 3s ===" >> /home/z/my-project/dev.log
   sleep 3
-  rm -rf /home/z/my-project/.next/cache 2>/dev/null
 done
